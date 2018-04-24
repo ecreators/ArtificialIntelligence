cb2118a8cba8109684e351ad31019982499dc939
devbrain-it <devbrain.it.solutions@gmail.com>
2018-04-24 06:44:05 +0200

    ai-15-fix project reboot #15
    Das Java Projekt als leer und dann mit maven zu erweitern war nicht gut für IntelliJ. Muss das Projekt neuaufsetzen.
    Sorry - Source bleibt erhalten
    
    (cherry picked from commit 4732f47)

--- src/main/java/de/ecr/ai/model/Layer.java
+++ src/main/java/de/ecr/ai/model/Layer.java
@@ -5,7 +5,6 @@ import de.ecr.ai.model.neuron.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.IntStream;
 
 import static java.text.MessageFormat.format;
 import static java.util.stream.Collectors.toList;
@@ -34,130 +33,169 @@ public final class Layer {
 	TODO call train set desired values for output
 	TODO in TEST-Method handle, when to stop iteration, if so doing
 	 */
-	
-	private final String        name;
-	private final List<Neuron>  neurons;
-	private       NeuronType    type;
-	private final NeuralNetwork network; // for later commits "back propagation"
-	
-	public Layer(String name, NeuralNetwork network) {
-		this.name = name;
-		this.network = network;
-		this.neurons = new ArrayList<>();
-	}
-	
-	/**
-	 * Empties neurons list
-	 */
-	public void clear() {
-		this.neurons.clear();
-	}
-	
-	/**
-	 * Appends new neurons (no softmax on output)
-	 */
-	public Layer createNeurons(int neuronsCount, NeuronType type, boolean softmax) {
-		this.type = type;
-		
-		Function<Integer, Neuron> builder = detectNeuronBuilder(type, softmax);
-		
-		IntStream.range(0, neuronsCount).parallel().mapToObj(builder::apply).forEach(this.neurons::add);
-		
-		// to append bindFullMesh
-		return this;
-	}
-	
-	/**
-	 * Returns the layer neuron type used to create neurons on this layer
-	 */
-	public NeuronType getType() {
-		return type;
-	}
-	
-	/**
-	 * Bind any neuron in this layer to each neuron in the parent layer neurons as a fully mesh.
-	 * Requires a layer of type {@link NeuronType#INPUT} or {@link NeuronType#HIDDEN}
-	 */
-	public void bindFullMesh(Layer parentLayer) {
-		if(type != NeuronType.OUTPUT) {
-			neurons.parallelStream()
-			       .map(n -> (IPropagateBack) n)
-			       .forEach(n -> bindToLayerNeurons(parentLayer, n));
-		} else {
-			throw new IllegalArgumentException("Your parent layer is output layer!");
-		}
-	}
-	
-	/**
-	 * Fetch neurons inputs, sum them and update output to all neurons in here. Do it parallel, if you like.
-	 */
-	public void propagate() {
-		neurons.parallelStream().forEach(Neuron::propagate);
-	}
-	
-	/**
-	 * Returns any output value in order of neurons
-	 */
-	public float[] getOutputs() {
-		float[] result = new float[neurons.size()];
-		for (int i = 0; i < neurons.size(); i++) {
-			Neuron neuron = neurons.get(i);
-			result[i] = neuron.getOutputValue();
-		}
-		return result;
-	}
-	
-	private void bindToLayerNeurons(Layer parentLayer, IPropagateBack n) {
-		List<IBindableSourceNeuron> sourceNeurons = parentLayer.neurons.parallelStream()
-		                                                               .map(p -> (IBindableSourceNeuron) p)
-		                                                               .collect(toList());
-		n.bindToInputNeurons(sourceNeurons);
-	}
-	
-	/**
-	 * Identifies the building process to separate neuron types in class
-	 */
-	private Function<Integer, Neuron> detectNeuronBuilder(NeuronType type, boolean softmax) {
-		Function<Integer, Neuron> builder;
-		
-		switch (type) {
-			case HIDDEN:
-				builder = i -> new HiddenNeuron(format("{0}{1}/H{2}", type.toString(), name, String.valueOf(i)), this);
-				break;
-			case INPUT:
-				builder = i -> new InputNeuron(format("{0}{1}/I{2}", type.toString(), name, String.valueOf(i)), this);
-				break;
-			case OUTPUT:
-				builder = i -> new OutputNeuron(format("{0}{1}/O{2}", type.toString(), name, String.valueOf(i)), softmax, this);
-				break;
-			default:
-				throw new IllegalArgumentException("Missing neuron type: " + type);
-		}
-		return builder;
-	}
-	
-	/**
-	 * Returns the number of neurons in this layer
-	 */
-	public int countNeurons() {
-		return neurons.size();
-	}
-	
-	/**
-	 * Returns weights to each neuron in this layer and it own weight to it input bindings
-	 */
-	public List<List<Float>> getWeights() {
-		return neurons.stream()
-		              .map(Neuron::getWeights)
-		              .collect(toList());
-	}
-	
-	/**
-	 * Returns biases to each neuron
-	 */
-	public List<Float> getBiases() {
-		return neurons.stream()
-		              .map(Neuron::getBias)
-		              .collect(toList());
-	}
+
+    private final String name;
+    private final List<Neuron> neurons;
+    private NeuronType type;
+    private final NeuralNetwork network; // for later commits "back propagation"
+
+    public Layer(String name, NeuralNetwork network) {
+        this.name = name;
+        this.network = network;
+        this.neurons = new ArrayList<>();
+    }
+
+    /**
+     * Empties neurons list
+     */
+    public void clear() {
+        this.neurons.clear();
+    }
+
+    /**
+     * Appends new neurons (no softmax on output)
+     */
+    public Layer createNeurons(int neuronsCount, NeuronType type, boolean softmax) {
+        this.type = type;
+
+        Function<Integer, Neuron> builder = detectNeuronBuilder(type, softmax);
+
+        for (int i = 0; i < neuronsCount; i++) {
+            Neuron neuron = builder.apply(i);
+            if (neuron == null) {
+                throw new RuntimeException("Builder invalid! It created a null-neuron!");
+            }
+            neurons.add(neuron);
+        }
+
+        // to append bindFullMesh
+        int removedNeurons = 0;
+        for (int i = 0; i < neurons.size(); i++) {
+            if (neurons.get(i) == null) {
+                neurons.remove(i--);
+                removedNeurons++;
+            }
+        }
+        if (removedNeurons > 0) {
+            throw new RuntimeException(removedNeurons + " null-neurons built!");
+        }
+
+        if (neuronsCount > neurons.size()) {
+            throw new RuntimeException("Issues during creating neurons! count not as expected");
+        }
+        return this;
+    }
+
+    /**
+     * Returns the layer neuron type used to create neurons on this layer
+     */
+    public NeuronType getType() {
+        return type;
+    }
+
+    /**
+     * Bind any neuron in this layer to each neuron in the parent layer neurons as a fully mesh.
+     * Requires a layer of type {@link NeuronType#INPUT} or {@link NeuronType#HIDDEN}
+     */
+    public Layer bindFullMesh(Layer parentLayer) {
+        if (parentLayer.getType() != NeuronType.OUTPUT) {
+            if (neurons.isEmpty()) {
+                throw new RuntimeException("Cannot bind on empty set of neurons. Neurons undefined!");
+            }
+            int i = 0;
+            for (Neuron neuron : neurons) {
+                if (neuron instanceof IPropagateBack) {
+                    bindToLayerNeurons(parentLayer, (IPropagateBack) neuron);
+                    i++;
+                }
+            }
+            if (i < neurons.size()) {
+                throw new RuntimeException("Cannot bind new neurons to parent layer. Neuron is not of type "
+                        + IPropagateBack.class.getCanonicalName());
+            }
+        } else {
+            throw new IllegalArgumentException("Your parent layer is output layer!");
+        }
+        return this;
+    }
+
+    /**
+     * Fetch neurons inputs, sum them and update output to all neurons in here. Do it parallel, if you like.
+     */
+    public void propagate() {
+        neurons.parallelStream().forEach(Neuron::propagate);
+    }
+
+    /**
+     * Returns any output value in order of neurons
+     */
+    public float[] getOutputs() {
+        float[] result = new float[neurons.size()];
+        for (int i = 0; i < neurons.size(); i++) {
+            Neuron neuron = neurons.get(i);
+            result[i] = neuron.getOutputValue();
+        }
+        return result;
+    }
+
+    private void bindToLayerNeurons(Layer parentLayer, IPropagateBack n) {
+        List<IBindableSourceNeuron> sourceNeurons = new ArrayList<>();
+        for (Neuron neuron : parentLayer.neurons) {
+            if (neuron instanceof IBindableSourceNeuron) {
+                sourceNeurons.add((IBindableSourceNeuron) neuron);
+            }
+        }
+        n.bindToInputNeurons(sourceNeurons);
+    }
+
+    /**
+     * Identifies the building process to separate neuron types in class
+     */
+    private Function<Integer, Neuron> detectNeuronBuilder(NeuronType type, boolean softmax) {
+        Function<Integer, Neuron> builder;
+
+        switch (type) {
+            case HIDDEN:
+                builder = i -> new HiddenNeuron(format("{0}{1}/H{2}", type.toString(), name, String.valueOf(i)), this);
+                break;
+            case INPUT:
+                builder = i -> new InputNeuron(format("{0}{1}/I{2}", type.toString(), name, String.valueOf(i)), this);
+                break;
+            case OUTPUT:
+                builder = i -> new OutputNeuron(format("{0}{1}/O{2}", type.toString(), name, String.valueOf(i)), softmax, this);
+                break;
+            default:
+                throw new IllegalArgumentException("Missing neuron type: " + type);
+        }
+        return builder;
+    }
+
+    /**
+     * Returns the number of neurons in this layer
+     */
+    public int countNeurons() {
+        return neurons.size();
+    }
+
+    /**
+     * Returns weights to each neuron in this layer and it own weight to it input bindings
+     */
+    public List<List<Float>> getWeights() {
+        return neurons.stream()
+                .map(Neuron::getWeights)
+                .collect(toList());
+    }
+
+    /**
+     * Returns biases to each neuron
+     */
+    public List<Float> getBiases() {
+        return neurons.stream()
+                .map(Neuron::getBias)
+                .collect(toList());
+    }
+
+    static List<Neuron> getNeurons(Layer layer) {
+        return layer.neurons;
+    }
 }
--- src/main/java/de/ecr/ai/model/neuron/Neuron.java
+++ src/main/java/de/ecr/ai/model/neuron/Neuron.java
@@ -24,135 +24,149 @@ import static java.util.stream.Collectors.toList;
  * @author Bjoern Frohberg
  */
 public abstract class Neuron {
-	
-	private       float               output;
-	private       float               bias;
-	private       IActivationFunction activation = IActivationFunction.SIGMOID;
-	final         List<Binding>       inputBindings;
-	private final String              name;
-	private final Layer               layer;
-	
-	protected Neuron(String name, Layer ownerLayer) {
-		this.name = name;
-		this.layer = ownerLayer;
-		this.inputBindings = new ArrayList<>();
-	}
-	
-	public final Layer getLayer() {
-		return layer;
-	}
-	
-	/**
-	 * Return a set output value
-	 */
-	public final float getOutputValue() {
-		return output;
-	}
-	
-	/**
-	 * Returns a set bias. A bias is a virtual offset to fix zero calculations.<br/>
-	 * It shifts the error calculation into a better range when calculating with zeros.
-	 */
-	@LearningData
-	public final float getBias() {
-		return bias;
-	}
-	
-	/**
-	 * Set a bias
-	 */
-	public void setBias(float bias) {
-		this.bias = bias;
-	}
-	
-	/**
-	 * Returns an activation function
-	 */
-	public final IActivationFunction getActivation() {
-		return activation;
-	}
-	
-	/**
-	 * If required to change the activation function
-	 */
-	public final void setActivation(IActivationFunction activation) {
-		this.activation = activation;
-	}
-	
-	/**
-	 * Simple identification for this neuron prototype
-	 */
-	public final NeuronType getType() {
-		if(this instanceof OutputNeuron) {
-			return NeuronType.OUTPUT;
-		}
-		if(this instanceof InputNeuron) {
-			return NeuronType.INPUT;
-		}
-		return NeuronType.HIDDEN;
-	}
-	
-	/**
-	 * Update output value based on inputs
-	 */
-	public void propagate() {
-		float inputSum = (float) (inputBindings.parallelStream().mapToDouble(Binding::calculateOutput).sum() + bias);
-		this.output = activation.activate(inputSum);
-	}
-	
-	/**
-	 * Calculates an error value pending on child neuron output values and their depending input bindings
-	 */
-	protected abstract float calculateError();
-	
-	/**
-	 * Updates the output value to this neuron. Commonly this value is caluclated. <br/>
-	 * Only an input neuron directly sets this output as identity.
-	 */
-	protected final void setOutput(float value) {
-		this.output = value;
-	}
-	
-	/**
-	 * Returns the calculated error
-	 *
-	 * @deprecated Will be removed. Use {@link #calculateError()} instead. (#3)
-	 */
-	@Deprecated
-	public final float getError() {
-		return calculateError();
-	}
-	
-	/**
-	 * Set an error fixed
-	 *
-	 * @deprecated Will be removed. Dies nothing right now (#3)
-	 */
-	@Deprecated
-	public final void setError(float error) {
-		// TODO removed
-	}
-	
-	/**
-	 * Returns any input bindings with weights
-	 */
-	protected final List<Binding> getInputBindings() {
-		return inputBindings;
-	}
-	
-	/**
-	 * Returns the neuron name
-	 */
-	public String getName() {
-		return name;
-	}
-	
-	/**
-	 * Returns number weight of each input bindings
-	 */
-	public List<Float> getWeights() {
-		return inputBindings.stream()
-		                    .map(Binding::getWeight)
-		                    .collect(toList());
-	}
+
+    private float output;
+    private float bias;
+    private IActivationFunction activation = IActivationFunction.SIGMOID;
+    final List<Binding> inputBindings;
+    private final String name;
+    private final Layer layer;
+    private final NeuronType type;
+
+    protected Neuron(String name, Layer ownerLayer) {
+        this.name = name;
+        this.layer = ownerLayer;
+        this.inputBindings = new ArrayList<>();
+
+        if (this instanceof OutputNeuron) {
+            type = NeuronType.OUTPUT;
+        } else if (this instanceof InputNeuron) {
+            type = NeuronType.INPUT;
+        } else {
+            type = NeuronType.HIDDEN;
+        }
+    }
+
+    public final Layer getLayer() {
+        return layer;
+    }
+
+    /**
+     * Return a set output value
+     */
+    public final float getOutputValue() {
+        return output;
+    }
+
+    /**
+     * Returns a set bias. A bias is a virtual offset to fix zero calculations.<br/>
+     * It shifts the error calculation into a better range when calculating with zeros.
+     */
+    @LearningData
+    public final float getBias() {
+        return bias;
+    }
+
+    /**
+     * Set a bias
+     */
+    public void setBias(float bias) {
+        this.bias = bias;
+    }
+
+    /**
+     * Returns an activation function
+     */
+    public final IActivationFunction getActivation() {
+        return activation;
+    }
+
+    /**
+     * If required to change the activation function
+     */
+    public final void setActivation(IActivationFunction activation) {
+        this.activation = activation;
+    }
+
+    /**
+     * Simple identification for this neuron prototype
+     */
+    public final NeuronType getType() {
+        return type;
+    }
+
+    /**
+     * Update output value based on inputs
+     */
+    public void propagate() {
+
+        // get sum from input x weight
+        // add bias to sum
+        // send through activation function
+        // set output as value what comes from activation function
+
+        double sum = inputBindings
+                .parallelStream()
+                .mapToDouble(Binding::calculateOutput).sum();
+        sum += bias;
+
+        float inputSum = (float) sum;
+        this.output = activation.activate(inputSum);
+    }
+
+    /**
+     * Calculates an error value pending on child neuron output values and their depending input bindings
+     */
+    protected abstract float calculateError();
+
+    /**
+     * Updates the output value to this neuron. Commonly this value is caluclated. <br/>
+     * Only an input neuron directly sets this output as identity.
+     */
+    protected final void setOutput(float value) {
+        this.output = value;
+    }
+
+    /**
+     * Returns the calculated error
+     *
+     * @deprecated Will be removed. Use {@link #calculateError()} instead. (#3)
+     */
+    @Deprecated
+    public final float getError() {
+        return calculateError();
+    }
+
+    /**
+     * Set an error fixed
+     *
+     * @deprecated Will be removed. Dies nothing right now (#3)
+     */
+    @Deprecated
+    public final void setError(float error) {
+        // TODO removed
+    }
+
+    /**
+     * Returns any input bindings with weights
+     */
+    public final List<Binding> getInputBindings() {
+        return inputBindings;
+    }
+
+    /**
+     * Returns the neuron name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns number weight of each input bindings
+     */
+    public List<Float> getWeights() {
+        return inputBindings.stream()
+                .map(Binding::getWeight)
+                .collect(toList());
+    }
 }
